da20cc077e7f432f608e2a5d2d9c298c0bbb328a
tayyabkashmiriiqbal-dev <tayyabkashmiriiqbal@gmail.com>
2026-09-20 14:34:53 +0500

    I have Done

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ let temp = document.querySelector(".temp");
 let rain = document.querySelector(".rain");
 let humidity = document.querySelector(".humidity");
 let time =document.querySelector(".time");
-let Time=
+let img=document.querySelector("#weather-icon")
+let feelslike=document.querySelector(".feels-like")
+let condition=document.querySelector(".condition");
+let country =document.querySelector(".country");
+
+
 async function addwheather(event) {
   event.preventDefault();
 
@@ -14,13 +19,23 @@ async function addwheather(event) {
     alert("Please enter a city name");
     return;
   }
-  let output = await axios(
+  try {
+  let output = await axios.get(
     `https://api.weatherapi.com/v1/current.json?key=60e0a3d2f152486e950213038260606&q=${city}`,
   );
 
+  img.src = "https:" + output.data.current.condition.icon;
+  condition.innerHTML="Condition : "+output.data.current.condition.text; 
   temp.innerHTML=output.data.current.temp_c+"°C";
-  rain.innerHTML="Chances of raining : " + output.data.current.precip_mm + " mm"
+  rain.innerHTML = "Rain: " + output.data.current.precip_mm + " mm";
   humidity.innerHTML="Humidity :"+output.data.current.humidity+"%"
-  time.innerHTML="Time :"+Time
+  time.innerHTML = "Local Time: " + output.data.location.localtime;
+  feelslike.innerHTML="Feels Like: "+output.data.current.feelslike_c+"°C"; 
+  country.innerHTML="Country :"+output.data.location.country 
+  } catch (error) {
+     console.error("Error fetching weather data:", error);
+    alert("City nahi mili ya koi error aaya hai");
+  }
+
 
 }
